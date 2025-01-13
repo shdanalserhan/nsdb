20402d0d654b8831bec367de15d596cea4aca421
shdanalserhan <166877297+shdanalserhan@users.noreply.github.com>
2025-01-13 12:42:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -78,5 +78,5 @@ def results():
 def serve_static_files(filename):
     return send_from_directory(app.static_folder, filename)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
